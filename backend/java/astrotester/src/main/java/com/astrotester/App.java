e1ade24d705ca3a413a66237c8f82c28e4bccce5
chris4567890 <starchristian4@hotmail.com>
2026-02-04 10:24:45 +0100

    added basic python and java files

---
+++ backend/java/astrotester/src/main/java/com/astrotester/App.java
@@ -0,0 +1,18 @@
+package com.astrotester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var x = 0;
+        while (x <= 5){
+            System.out.println( x );
+            ++x;
+        }
+        ;
+    }
+}
