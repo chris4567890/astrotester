b2aae6750d173a45376db31796a704eedfe4190d
chris4567890 <starchristian4@hotmail.com>
2026-02-05 22:43:30 +0100

    changed search controller'

--- backend/java/astrotester/src/main/java/com/astrotester/restSecurity/controllers/QuerryController.java
+++ backend/java/astrotester/src/main/java/com/astrotester/restSecurity/controllers/QuerryController.java
@@ -1,7 +1,60 @@
 package com.astrotester.restSecurity.controllers;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Handler;
+
 public class QuerryController {
+
+    private static QuerryController instance;
+
     private QuerryController(){
         //private constructor for singleton
     }
+
+    public static QuerryController getInstance(boolean isTest){
+        if (instance == null){
+            instance = new QuerryController();
+        }
+        return instance;
+    }
+
+    public static Handler search(){
+        return ctx ->{
+            try {
+                String query = ctx.queryParam("q");
+                if(query == null || query.isBlank()){
+                    ctx.status(400);
+                    throw new ApiException(400,"missing query parameter");
+                }
+                String encodedQuery =
+                    URLEncoder.encode(query,StandardCharsets.UTF_8);
+                
+                    HttpClient client = HttpClient.newHttpClient();
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create"http://localhost:5000/hello?query=" + encodedQuery
+
+                            ))
+                            .GET()
+                            .build();
+                    
+                    HttpResponse<String> response =
+                            client.send(request,HttpResponse.BodyHandlers.ofString());
+                    
+                    ctx.json(response.body());        
+
+
+            }catch (Exception e){
+                ctx.status(500);
+                throw new ApiException(
+                        500,
+                        "error calling python service: "+ e.getMessage()
+                );
+            }
+        }
+    }
+
 }
