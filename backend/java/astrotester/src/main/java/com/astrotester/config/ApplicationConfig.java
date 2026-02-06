d5a78b7db122fd42cb88ffe8c969491ca7ae299b
chris4567890 <starchristian4@hotmail.com>
2026-02-06 17:04:55 +0100

    fixed some setup shit

--- backend/java/astrotester/src/main/java/com/astrotester/config/ApplicationConfig.java
+++ backend/java/astrotester/src/main/java/com/astrotester/config/ApplicationConfig.java
@@ -1,7 +1,27 @@
-package main.java.com.astrotester.config;
+package com.astrotester.config;
 
+import io.javalin.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 public class ApplicationConfig {
     
+    private static ObjectMapper jsonMapper = new ObjectMapper ();
+    private static ApplicationConfig appConfig;
     
-    
+    private Javalin app;
+
+    private ApplicationConfig (){}
+
+    public static ApplicationConfig getInstance(){
+        if(appConfig == null){
+            appConfig = new ApplicationConfig();        
+        }
+        return appConfig;
+    }
+
+    public ApplicationConfig initiateServer(){
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+        String separator = System.getProperty("file.separator");
+        app =  
+    };
+
 }
