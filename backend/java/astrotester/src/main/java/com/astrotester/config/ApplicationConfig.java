6e39ee9588a3873dd89b781caac2ef46023c38c0
chris4567890 <starchristian4@hotmail.com>
2026-02-06 16:42:09 +0100

    started application config

---
+++ backend/java/astrotester/src/main/java/com/astrotester/config/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package main.java.com.astrotester.config;
+
+public class ApplicationConfig {
+    
+    
+    
+}
--- backend/java/astrotester/src/main/java/com/astrotester/config/Routes.java
+++ backend/java/astrotester/src/main/java/com/astrotester/config/Routes.java
@@ -1,12 +1,19 @@
 package main.java.com.astrotester.config;
 
 import com.astrotester.restSecurity.controllers.QuerryController;
-
+import static io.javalin.apibuilder.ApiBuilder.*;
 public class Routes {
     private static QuerryController querryController ;
     
     public static EndpointGroup getRoutes(boolean isTesting){
         querryController = QuerryController.getInstance(isTesting);
+
+        return () -> {
+            path("/",() ->{
+                get("/search",QuerryController.search());
+            });
+        };
+
     }
 
 }
