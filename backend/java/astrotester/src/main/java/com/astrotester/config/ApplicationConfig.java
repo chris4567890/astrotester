c6873a43388dad0260277db6dfba0a71f448e297
chris4567890 <starchristian4@hotmail.com>
2026-02-06 19:11:10 +0100

    more setup with pom and configs

--- backend/java/astrotester/src/main/java/com/astrotester/config/ApplicationConfig.java
+++ backend/java/astrotester/src/main/java/com/astrotester/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package com.astrotester.config;
 
 import io.javalin.*;
+import io.javalin.apibuilder.EndpointGroup;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ApplicationConfig {
     
     private static ObjectMapper jsonMapper = new ObjectMapper ();
@@ -21,7 +24,38 @@ public class ApplicationConfig {
     public ApplicationConfig initiateServer(){
         System.out.println("Working directory: " + System.getProperty("user.dir"));
         String separator = System.getProperty("file.separator");
-        app =  
+        app = Javalin.create(config -> {
+            config.http.defaultContentType="application/json";
+            config.routing.contextPath="/api";
+            config.plugins.enableDevLogging();
+
+        }); 
+        return appConfig; 
     };
+
+    public ApplicationConfig setRoutes(EndpointGroup routes){
+        app.routes(routes);
+        return appConfig;
+    }
+
+    public ApplicationConfig startServer(int port){
+        app.start(port);
+        return appConfig;
+    }
+
+    public ApplicationConfig configureCors(){
+        app.before(ctx -> {
+            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            ctx.header("Access-Control-Allow-Headers", "Content-Type");
+        });
+
+        app.options("/*", ctx -> {
+            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            ctx.header("Access-Control-Allow-Headers", "Content-Type");
+        });
+        return appConfig;
+    }
 
 }
