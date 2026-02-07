7057b137fa49e770996b6ab1faf3f649f1067b0c
chris4567890 <starchristian4@hotmail.com>
2026-02-07 16:45:15 +0100

    preparing raect for connection to java

--- frontend/src/components/SearchField.jsx
+++ frontend/src/components/SearchField.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 
 export default function SearchField() {
-    const handleSubmit = (e) => {
+    const [query,setQuery] = useState('');
+    const [results, setResults] = useState(null);
+    const [error,setError] = useState(null);
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Form submitted!');
+        setLoading(true);
+        setError(null);
+        
+        try{
+            const response = await fetch(`http://localhost:7070/search?q=${encodeURIComponent(query)}`);
+            
+            if(!response.ok){
+                throw new Error(`Error:${response.status}`);
+            }
+            const data = await response.json();
+            setResults(data);
+            console.log('search result',data);
+        }catch(err){
+            setError(err.message);
+            console.log('error fetching data:',err)
+        }
     };
 
     return(
         <>
             <div id="searchField">
                 <form onSubmit={handleSubmit}>
-                    <input type="text" />
+                    <input 
+                        type="text" 
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        placeholder="enter object name"
+                    />
                     <br/>
                     <button type="submit">search</button>
                 </form>
